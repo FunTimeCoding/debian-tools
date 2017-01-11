bbad11e13c874a8cfa2774ed0e12d1679fe9a209
Alexander Reitzel <funtimecoding@gmail.com>
2017-01-11 13:08:37 +0100

    add script to get the current stable debian version

---
+++ bin/get-debian-version.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+OUTPUT=$(wget --quiet http://cdimage.debian.org/debian-cd/current/amd64/iso-cd --output-document -)
+IMAGES=$(echo ${OUTPUT} | hxnormalize -x | hxselect -s "\n" -c a | grep '^debian-[0-9].*') || true
+VERSION=$(echo "${IMAGES}" | head -1 | grep --only-match '[0-9]\+\.[0-9]\+\.[0-9]\+')
+
+echo "Debian: ${VERSION}"
