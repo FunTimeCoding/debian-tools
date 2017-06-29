5249c456c6fd0d403346742326578b03ff749dd7
Alexander Reitzel <funtimecoding@gmail.com>
2017-06-29 13:25:41 +0200

    attempt different syntax for package_data

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[],
     scripts=['bin/dt'],
     packages=['debian_tools'],
-    package_data={'template': ['*.j2']},
+    package_data={'template': ['template/*.j2']},
     author='Alexander Reitzel',
     author_email='funtimecoding@gmail.com',
     url='http://example.org',
