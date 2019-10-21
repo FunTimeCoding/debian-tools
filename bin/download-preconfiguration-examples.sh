e00f14f70f5deeb23f09ae0e8686fbcea6394bc2
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-21 12:05:31 +0200

    download buster preseed

--- bin/download-preconfiguration-examples.sh
+++ bin/download-preconfiguration-examples.sh
@@ -3,6 +3,6 @@
 DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 
-for RELEASE in jessie stretch; do
+for RELEASE in jessie stretch buster; do
     wget --output-document "${SCRIPT_DIRECTORY}/../debian_tools/template/example/${RELEASE}.cfg" "https://www.debian.org/releases/${RELEASE}/example-preseed.txt"
 done
